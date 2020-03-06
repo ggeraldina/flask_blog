b93b950f46c605ba787465367f15d8a2506c3f05
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-03-06 21:52:33 +0300

    Update error pages

--- app/errors.py
+++ app/errors.py
@@ -3,8 +3,8 @@ from flask import render_template
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('blog/page_not_found.html'), 404
+    return render_template('error_pages/page_not_found.html'), 404
 
 @app.errorhandler(500)
 def page_server_error(error):
-    return render_template('blog/page_server_error.html'), 500
+    return render_template('error_pages/page_server_error.html'), 500
