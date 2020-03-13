3300ab9a738c9b4e639f8ccfe37ac3ff81953127
Грушевская Дарья Владимировна <grushevskaya.d.v@yandex.ru>
2020-03-13 12:26:25 +0300

    Added the pylint

--- app/errors.py
+++ app/errors.py
@@ -1,5 +1,5 @@
-from app import app
 from flask import render_template
+from app import app
 
 @app.errorhandler(404)
 def page_not_found(error):
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-from app import app
 from flask import render_template
+from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
     msg = "Привет, замечательный человек!!!"
-    data = { "msg": msg }
+    data = {"msg": msg}
     return render_template('blog/index.html', data=data), 200
 
 @app.route("/<int:number>")
-def showNumber(number):
+def show_number(number):
     return "Your number: %d" % number
