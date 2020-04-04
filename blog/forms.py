137a9356f5e5aa0aa1c7634e30a48d199351d4a7
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-04-04 17:37:29 +0300

    Added a registration

--- blog/forms.py
+++ blog/forms.py
@@ -1,18 +1,46 @@
 """ This file contain all forms """
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo
+
+from . import MONGO
 
 
 class PostForm(FlaskForm):
     """ Class for a post form """
     title = StringField(label="Название:",
-                        validators=[DataRequired(), Length(max=200)],
-                        id="id_title")
-    text = TextAreaField(label="Текст:", validators=[DataRequired()], id="id_text")
+                        validators=[DataRequired(message="Поле с названием должно быть заполнено."), 
+                                    Length(max=200, message="Максимум 200 символов.")])
+    text = TextAreaField(label="Текст:", 
+                         validators=[DataRequired(message="Поле с текстом должно быть заполнено.")])
 
 class LoginForm(FlaskForm):
     """ Class for a login form """
-    username = StringField(label="Имя пользователя:", validators=[DataRequired()])
-    password = PasswordField(label="Пароль:", validators=[DataRequired()])
-    remember_me = BooleanField(label="Запомнить меня")
+    username = StringField(label="Имя пользователя:", 
+                           validators=[DataRequired(message="Введите имя пользователя."), 
+                                       Length(max=30, message="Максимум 30 символов.")])
+    password = PasswordField(label="Пароль:", 
+                             validators=[DataRequired(message="Введите пароль.")])
+
+    def validate_username(self, username):
+        user = MONGO.db.blog_user.find_one({"username": username.data})
+        if user is None:
+            raise ValidationError("Пользователь с таким именем не существует. \
+                                   Пожалуйста, попробуйте еще раз.")
+
+class RegistrationForm(FlaskForm):
+    username = StringField(label="Имя пользователя:", 
+                           validators=[DataRequired(message="Введите имя пользователя."), 
+                                       Length(max=30, message="Максимум 30 символов.")])
+    password = PasswordField(label="Пароль:", 
+                             validators=[DataRequired(message="Введите пароль.")])
+    password2 = PasswordField(label="Повторите пароль:", 
+                              validators=[DataRequired(message="Повторите пароль."), 
+                                          EqualTo(fieldname="password", 
+                                                  message="Пароль повторно введен неверно.")])
+
+    def validate_username(self, username):
+        user = MONGO.db.blog_user.find_one({"username": username.data})
+        if user is not None:
+            raise ValidationError("Пользователь с таким именем уже существует. \
+                                   Пожалуйста, выберите другое имя.")
--- blog/views.py
+++ blog/views.py
@@ -4,28 +4,33 @@ from datetime import datetime
 from functools import wraps
 
 from flask import flash, redirect, render_template, request, session, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.urls import url_parse
 
 from . import APP, MONGO, NETLOC
-from .forms import LoginForm, PostForm
+from .forms import LoginForm, PostForm, RegistrationForm
 
 
 @APP.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == "admin" and secrets.compare_digest(form.password.data, "admin"):
-            session["logged_in"] = True
-            next_page = request.args.get("next")
-            if not next_page or url_parse(next_page).netloc != NETLOC:
-                next_page = url_for("post_list")
-            return redirect(next_page)
-        flash("Неправильное имя или пароль. Пожалуйста, попробуйте еще раз.")
+        user = MONGO.db.blog_user.find_one({"username": form.username.data})
+        if user is None or not check_password_hash(user["password"], form.password.data):
+            flash("Неправильное имя или пароль. Пожалуйста, попробуйте еще раз.")
+            return redirect(url_for("login"))
+        session["logged_in"] = True
+        session["username"] = user["username"]
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != NETLOC:
+            next_page = url_for("post_list")
+        return redirect(next_page)
     return render_template("registration/login.html", form=form)
 
 @APP.route("/logout")
 def logout():
     session["logged_in"] = False
+    session.pop("username", None)
     return redirect(url_for("post_list"))
 
 def login_required(func):
@@ -36,6 +41,18 @@ def login_required(func):
             return redirect(url_for("login", next=request.url))
         return func(*args, **kwargs)
     return wrapper
+
+@APP.route("/register", methods=["GET", "POST"])
+def register():    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = {"username": form.username.data,
+                "password": generate_password_hash(form.password.data),
+                "create_date": datetime.utcnow().isoformat()}
+        user["_id"] = MONGO.db.blog_user.insert_one(user).inserted_id
+        flash("Поздравляем! Вы успешно зарегистрированы")
+        return redirect(url_for("login"))
+    return render_template("registration/register.html", form=form)
 
 @APP.route("/")
 def post_list():
@@ -58,7 +75,7 @@ def post_new():
                 "published_date": None,
                 "title": form.title.data,
                 "text": form.text.data,
-                "author": "author"}
+                "author": session.get("username", "unknown_author")}
         post["_id"] = MONGO.db.blog_post.insert_one(post).inserted_id
         return redirect(url_for("post_detail", primary_key=post["_id"]))
     return render_template("blog/post_edit.html", form=form)
@@ -88,6 +105,7 @@ def post_draft_list():
 def post_publish(primary_key):
     post = MONGO.db.blog_post.find_one_or_404({"_id": primary_key})
     post["published_date"] = datetime.utcnow().isoformat()
+    post["published_author"] = session.get("username", "unknown_author")
     MONGO.db.blog_post.update_one({"_id": primary_key}, {"$set": post})
     return redirect(url_for("post_detail", primary_key=primary_key))
 
